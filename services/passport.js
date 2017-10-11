9f993a2de4418d56bd0f4e30d06e66ae5b1cb040
Richie Yi <helloworldrichie@gmail.com>
2017-10-10 22:01:19 -0700

    Set up Mongoose to find an existing user or create a new one

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+// Pull User model out of Mongoose
+const User = mongoose.model('users');
 
 // Tell Passport to use Google oauth strategy
 passport.use(
@@ -12,9 +16,20 @@ passport.use(
 		},
 		// Do something with received access token
 		(accessToken, refreshToken, profile, done) => {
-			console.log('access', accessToken);
-			console.log('refresh', refreshToken);
-			console.log('profile', profile);
+			// Find possible existing user in DB
+			User.findOne({ googleId: profile.id }).then(existingUser => {
+				// We already have a record with given ID
+				if (existingUser) {
+					done(null, existingUser);
+				}
+				// We don't have a record with this ID
+				new User({
+					googleId: profile.id
+				})
+					// Save new user to Mongo
+					.save()
+					.then(user => done(null, user));
+			});
 		}
 	)
 );
