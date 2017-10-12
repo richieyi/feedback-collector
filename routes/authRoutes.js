d153b0466d8c913306a9782bcad5b1ba432b7e90
Richie Yi <helloworldrichie@gmail.com>
2017-10-11 22:52:50 -0700

    Fix current user route and add logout route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,7 +11,12 @@ module.exports = app => {
 	// Redirect user to server
 	app.get('/auth/google/callback', passport.authenticate('google'));
 
-	app.get('/auth/current_user', (req, res) => {
+	app.get('/api/logout', (req, res) => {
+		req.logout();
+		res.send(req.user);
+	});
+
+	app.get('/api/current_user', (req, res) => {
 		res.send(req.user);
 	});
 };
