1b4cce4ca6a5f3792af1660d000a5408f57047a1
Richie Yi <helloworldrichie@gmail.com>
2017-10-16 21:01:51 -0700

    Begin React Router setup and create dummy route components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-	return <div>Hi there!</div>;
+	return (
+		<div>
+			<Router>
+				<div className="container">
+					<Header />
+					<Route exact path="/" component={Landing} />
+					<Route exact path="/surveys" component={Dashboard} />
+					<Route path="/surveys/new" component={SurveyNew} />
+				</div>
+			</Router>
+		</div>
+	);
 };
 
 export default App;
