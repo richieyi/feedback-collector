67f4bc77ac800b43712a6baa2707d4b419d7979c
Richie Yi <helloworldrichie@gmail.com>
2017-10-17 13:09:51 -0700

    Downgrade mongo to fix error and refactor fetchUser action to use async/await

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,13 +1,8 @@
 import axios from 'axios';
 import { FETCH_USER } from './actionTypes.js';
 
-export const fetchUser = () => {
-	return dispatch => {
-		axios.get('/api/current_user').then(res =>
-			dispatch({
-				type: FETCH_USER,
-				payload: res
-			})
-		);
-	};
+export const fetchUser = () => async dispatch => {
+	const res = await axios.get('/api/current_user');
+	
+	dispatch({ type: FETCH_USER, payload: res });
 };
